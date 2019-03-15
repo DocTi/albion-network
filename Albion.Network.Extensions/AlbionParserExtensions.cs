4235056c53dd907e4632fab284275e2f765bca91
Maxim Sirotkin <d.zh2012@yandex.ru>
2019-03-16 02:28:22 +0300

    init projects

---
+++ Albion.Network.Extensions/AlbionParserExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Albion.Network.Extensions
+{
+    public static class AlbionParserExtensions
+    {
+        // TODO
+    }
+}
