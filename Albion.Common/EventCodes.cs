50075c002a5e5dc1ecd1d240e756f953cbfbb0fe
Maxim Sirotkin <d.zh2012@yandex.ru>
2019-03-16 02:37:45 +0300

    move handler

--- Albion.Common/EventCodes.cs
+++ Albion.Common/EventCodes.cs
@@ -1,4 +1,7 @@
-﻿namespace Albion.Common
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+namespace Albion.Common
 {
     public enum EventCodes : short
     {
