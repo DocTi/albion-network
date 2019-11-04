f7208755bd7c3612df28716a96071498a3500c3e
Maxim <d.zh2012@yandex.ru>
2019-11-05 01:08:36 +0300

    debug

---
+++ Albion.Event/DebugEvent.cs
@@ -0,0 +1,15 @@
+﻿using Albion.Network;
+using System.Collections.Generic;
+
+namespace Albion.Event
+{
+    public class DebugEvent : BaseEvent
+    {
+        public DebugEvent(Dictionary<byte, object> parameters) : base(parameters)
+        {
+            Parameters = parameters;
+        }
+
+        public Dictionary<byte, object> Parameters { get; }
+    }
+}
