148347f2d72305b163ceb133f32c8661f075e19c
DocTi <d.zh2012@yandex.ru>
2020-07-25 20:36:05 +0300

    events

---
+++ Albion.Event/DetachItemContainerEvent.cs
@@ -0,0 +1,16 @@
+﻿using Albion.Network;
+using System;
+using System.Collections.Generic;
+
+namespace Albion.Event
+{
+    public class DetachItemContainerEvent : BaseEvent
+    {
+        public DetachItemContainerEvent(Dictionary<byte, object> parameters) : base(parameters)
+        {
+            ContainerId = new Guid((byte[])parameters[0]);
+        }
+
+        public Guid ContainerId { get; }
+    }
+}
